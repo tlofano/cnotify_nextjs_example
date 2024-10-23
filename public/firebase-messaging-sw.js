44fd404934a1ff673136521f0e6a60ada66e8f53
Tomás Lofano <tomilofano@gmail.com>
2024-10-23 15:21:56 -0300

    feat: update dependencies

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -20,11 +20,4 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
   console.log('Received background message ', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
 });
